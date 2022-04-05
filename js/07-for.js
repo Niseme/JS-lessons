23f5b3293fc548e1645a5db21dd2794bcce5f701
Niseme <nijolesemele@gmail.com>
2022-04-05 16:48:03 +0300

    for ciklai

---
+++ js/07-for.js
@@ -0,0 +1,8 @@
+const pazymiai = [10, 10, 5, 10, 10];
+let sum = 0
+for (let i = 0; i < pazymiai.length; i++) {
+    sum += pazymiai[i]
+}
+const vidurkis = sum / pazymiai.length;
+console.log('Vidurkis:', vidurkis);
+
