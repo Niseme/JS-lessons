fae240f25c0e14d10f238cef2c56589182a300bf
Niseme <nijolesemele@gmail.com>
2022-04-05 16:22:40 +0300

    if nesting

--- js/05-if.js
+++ js/05-if.js
@@ -2,8 +2,9 @@
 IF
 
 Sablonai:
-if () {}
-if () {} else {}
+- if () {}
+- if () {} else {}
+- 
 
 Palyginimo operatoriai:
 - visi: <, >, <=, >=, ==, !=, ===, !==
@@ -27,3 +28,70 @@ console.log(tt.length);
 const ee = [1, 2, 5];
 console.log(ee.length);
 
+const day = 4;
+if (day === 1) {
+    console.log('Pirmadienis');
+} else if (day === 2) {
+    console.log('Antradienis');
+} else if (day === 3) {
+    console.log('treciadienis');
+}
+else if (day === 4) {
+    console.log('ketvirtadienis');
+}
+else if (day === 5) {
+    console.log('penktadienis');
+}
+else if (day === 6) {
+    console.log('sestadienis');
+}
+else if (day === 7) {
+    console.log('sekmadienis');
+} else {
+    console.log('tokia diena neegzistuoja');
+}
+
+
+console.log('----------------------');
+
+const akiuSpalva = 'balta'
+
+if (akiuSpalva === 'zalia') {
+    console.log('Zaliaakiai yra zalciai');
+} else {
+    if (akiuSpalva === 'melyna') {
+        console.log('melynakiai megsta melynes');
+    } else {
+        if (akiuSpalva === 'ruda') {
+            console.log('rudaakiai yra melagiai');
+        } else {
+            if (akiuSpalva === 'raudona') {
+                console.log('tu pavardges');
+            } else {
+                console.log('rinkis kita spalva');
+            }
+        }
+    }
+}
+
+console.log('----------------------');
+
+const temperatura = 25;
+const nuoKadaSilta = 15;
+const arLyja = true;
+
+if (arLyja === true) {
+    if (temperatura >= nuoKadaSilta) {
+        console.log('silta ir lyja');
+    } else {
+        console.log('salta ir lyja');
+    }
+
+} else {
+    if (temperatura >= nuoKadaSilta) {
+        console.log('gali nesirengti');
+    } else {
+        console.log('uzsidek striuke');
+    }
+
+}
