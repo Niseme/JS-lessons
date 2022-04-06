37ceaa7ad17650ea9dd331f13676736d976bb7e8
Niseme <nijolesemele@gmail.com>
2022-04-06 19:20:11 +0300

    for cicle

--- js/07-for.js
+++ js/07-for.js
@@ -1,8 +1,40 @@
-const pazymiai = [10, 10, 5, 10, 10];
+/*const pazymiai = [10, 10, 5, 10, 10];
 let sum = 0
 for (let i = 0; i < pazymiai.length; i++) {
     sum += pazymiai[i]
 }
 const vidurkis = sum / pazymiai.length;
-console.log('Vidurkis:', vidurkis);
+console.log('Vidurkis:', vidurkis);*/
 
+//suma nuo 0 iki 0
+
+/*let sum = 0;
+for (let i = 0; i <= 0; i++) {
+    sum += i
+}
+console.log(sum);*/
+
+//suma nuo 0 iki 4
+
+/*let sum = 0;
+for (let i = 0; i <= 4; i++) {
+    sum += i
+}
+console.log(sum);*/
+
+//suma nuo 4 iki 0
+/*
+let sum = 0;
+for (let i = 4; i >= 0; i--) {
+    sum += i
+}
+console.log(sum);*/
+
+
+//zodiz atvirksciai
+/*let text = 'hello'
+let newText = '';
+for (let i = 0; i <= text.length; ++i) {
+    newText = newText + text[text.length - i];
+}
+console.log(newText);*/
