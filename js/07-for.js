5efda7c4bfff525e96e1cbd09a1cbf82b872b087
Niseme <nijolesemele@gmail.com>
2022-04-08 14:35:06 +0300

    for

--- js/07-for.js
+++ js/07-for.js
@@ -1,3 +1,5 @@
+/*
+ */
 /*const pazymiai = [10, 10, 5, 10, 10];
 let sum = 0
 for (let i = 0; i < pazymiai.length; i++) {
@@ -32,9 +34,45 @@ console.log(sum);*/
 
 
 //zodiz atvirksciai
-/*let text = 'hello'
+let text = 'hello'
 let newText = '';
-for (let i = 0; i <= text.length; ++i) {
+/*for (let i = 1; i <= text.length; ++i) {
     newText = newText + text[text.length - i];
 }
 console.log(newText);*/
+
+/*for (let i = text.length; i > 0; i--) {
+    newText = newText + text[i - 1];
+}
+console.log(newText);*/
+
+/*//WHILE
+const pazymiai = [10, 10, 5, 10, 10];
+let suma = 0;
+let i = 0;
+while (i < pazymiai.length) {
+    suma += pazymiai[i]
+    i++;
+    console.log(i, suma);
+}*/
+
+//FOR-OF
+/*const pazymiai = [10, 10, 5, 10, 10];
+let suma = 0
+for (const pazymys of pazymiai) {
+    suma += pazymys;
+    console.log(suma);
+}*/
+
+/*//FOREACH
+const pazymiai = [10, 10, 5, 10, 10];
+let sum = 0;
+pazymiai.forEach((pazymys, i) => {
+    sum += pazymys;
+    console.log(sum);
+});*/
+
+//FOR-IN skirtas objektams, bet ne masyvams
+
+
+
